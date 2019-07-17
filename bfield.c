a4809a6cc07ab60826e089b5edcc952bcee50f12
Luquant Singh <lsingh@sunfire08.pppl.gov>
2019-07-17 16:35:25 -0400

    Fixed indexing bug in single filament calculation and added multifilament field calculation

--- bfield.c
+++ bfield.c
@@ -15,7 +15,9 @@ double* nsurfx;
 double* nsurfy;
 double* nsurfz;
 
-size_t size_surf;
+int Nzeta;
+int Nteta;
+
 double* currents;
 int Nseg;
 int Ncoils;
@@ -46,7 +48,7 @@ void CalculateSingleField(double x, double y, double z, \
    bz = 0.0;
    
    for(i=0;i<Ncoils;i++){
-      for(j=0;j<(Nseg+1);j++){
+      for(j=0;j<Nseg;j++){
 
          l = sqrt( pow( ((*(sfilx+i*(Nseg+1)+j+1)) - (*(sfilx+i*(Nseg+1)+j))) ,2) + \
                    pow( ((*(sfily+i*(Nseg+1)+j+1)) - (*(sfily+i*(Nseg+1)+j))) ,2) + \
@@ -87,25 +89,63 @@ void CalculateSingleField(double x, double y, double z, \
 
 
          
-void CalcMultiFilsB(double x, double y, double z, \
+void CalculateMultiField(double x, double y, double z, \
                           double* Bx, double* By, double* Bz){
 
-
-  int i,j,k;
-  int Nfils; 
-
+   double muc = 1.0e-7;
+   int i,j,k;
+   int Nfils = Nradfil*Ntorfil; 
+   double l, ex, ey, ez, bx, by, bz;
+   double ri, rix, riy, riz;
+   double rf, rfx, rfy, rfz;
+   
+   bx = 0.0;
+   by = 0.0;
+   bz = 0.0;
+   
    for(i=0;i<Ncoils;i++){
       for(j=0;j<Nfils;j++){
-         for(k=0;k<Nseg+1;k++){
+         for(k=0;k<Nseg;k++){
+            l = sqrt( pow( ((*(mfilx+i*Nfils*(Nseg+1)+j*(Nseg+1)+k+1)) - (*(mfilx+i*Nfils*(Nseg+1)+j*(Nseg+1)+k)) ) ,2) + \
+                      pow( ((*(mfily+i*Nfils*(Nseg+1)+j*(Nseg+1)+k+1)) - (*(mfily+i*Nfils*(Nseg+1)+j*(Nseg+1)+k)) ) ,2) + \
+                      pow( ((*(mfilz+i*Nfils*(Nseg+1)+j*(Nseg+1)+k+1)) - (*(mfilz+i*Nfils*(Nseg+1)+j*(Nseg+1)+k)) ) ,2) ); 
+               
+            ex = ( (*(mfilx+i*Nfils*(Nseg+1)+j*(Nseg+1)+k+1) ) - (*(mfilx+i*Nfils*(Nseg+1)+j*(Nseg+1)+k)) ) / l;  
+            ey = ( (*(mfily+i*Nfils*(Nseg+1)+j*(Nseg+1)+k+1) ) - (*(mfily+i*Nfils*(Nseg+1)+j*(Nseg+1)+k)) ) / l;    
+            ez = ( (*(mfilz+i*Nfils*(Nseg+1)+j*(Nseg+1)+k+1) ) - (*(mfilz+i*Nfils*(Nseg+1)+j*(Nseg+1)+k)) ) / l;   
+
+
+            rix = x - ( *(mfilx+i*Nfils*(Nseg+1)+j*(Nseg+1)+k) );
+            riy = y - ( *(mfily+i*Nfils*(Nseg+1)+j*(Nseg+1)+k) );
+            riz = z - ( *(mfilz+i*Nfils*(Nseg+1)+j*(Nseg+1)+k) );
+             ri = sqrt( pow(rix,2) + pow(riy,2) + pow(riz,2) );
+
+
+            rfx = x - ( *(mfilx+i*Nfils*(Nseg+1)+j*(Nseg+1)+k+1) ); 
+            rfy = y - ( *(mfily+i*Nfils*(Nseg+1)+j*(Nseg+1)+k+1) );  
+            rfz = z - ( *(mfilz+i*Nfils*(Nseg+1)+j*(Nseg+1)+k+1) );   
+             rf = sqrt( pow(rfx,2) + pow(rfy,2) + pow(rfz,2) );
+ 
+         
+             bx += muc * ( *(currents+i) / Nfils ) * (ey*riz-ez*riy) * (2*l*(ri+rf)) \
+                  / ( (ri*rf) * ( pow((ri+rf),2) - pow(l,2)) );
+
+             by += muc * ( *(currents+i) / Nfils ) * (ez*rix-ex*riz) * (2*l*(ri+rf)) \
+                  / ( (ri*rf) * ( pow((ri+rf),2) - pow(l,2)) );
+
+             bz += muc * ( *(currents+i) / Nfils ) * (ex*riy-ey*rix) * (2*l*(ri+rf)) \
+                  / ( (ri*rf) * ( pow((ri+rf),2) - pow(l,2))  );
+            
             
          }
       }
    }
-
+   
+   *Bx = bx;
+   *By = by;
+   *Bz = bz;
 
 }
-//void DiagnoseSingleFilsB(void){}
-//void DiagnoseMultiFilsB(void){}
 
 
 
