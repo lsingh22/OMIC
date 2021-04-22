7af18d4b972d051eeb80114577001327e8aec343
Luquant Singh <lsingh@sunfire06.pppl.gov>
2021-04-22 00:17:46 -0400

    Add pseudocode and host-side code to gpu field calculation

--- bfield_gpu.cu
+++ bfield_gpu.cu
@@ -20,18 +20,40 @@ __global__ void field_kernel(void) {
 
 }
 
-__global__ void magnetic_field(double* bx, double* by, double* bz, const double* currents, const double* mfx, const double* mfy, const int nseg) {
+__host__ void magnetic_field(double* bx, double* by, double* bz, const double* currents, const double* mfx, const double* mfy, const double* mfz, const int ncoil, const int nseg) {
 
-	// Dynamically allocate shared memory
+	// TODO: for purposes of parallel implementation, ncoil should be Ncoil * Nfils
 
-	// Set up execution configuration
+	// Define execution configuration for field_kernel call
+	// A 2D block configuration enables easy tracking of coils, segments
+	// A 1D grid configuration is determined by ty, the number of coils / block
+	// Field kernel computes B-field due to tx-th segment of ty-th coil
+
+	unsigned int ty_dim = (1024 + nseg - 1) / nseg; // number of coils per block
+	unsigned int tx_dim = ty_dim * nseg; // number of segments per coil
+	unsigned int DimGrid = (ncoil + ty_dim - 1) / ty_dim;
+
+	dim3 DimBlock(tx_dim, ty_dim);
+
+	// Create helper currents array that stores each filament's current
+	double* Icoil;
+	
+	// Fill with currents of each filament
+
+	cudaMallocManaged((void**)&Icoil, ncoil * sizeof(double));
 
 	// For each point on magnetic surface,
 
-		// Call field kernel to get magnetic field 
+		// Call field kernel to get magnetic field
+		// field_kernel<<< >>>(mfx[i], mfy[i], mfz[i], Icoil);
+		// cudaDeviceSynchronize(); // synchronize to prepare for reduction
+
+	// Call a parallel reduction to get B-total at each point
 
 	// Call rotation kernel to reflect results to all field periods
 
 	// Cleanup
+	cudaFree(Icoil);
 }
+
 
--- bfield_gpu.cuh
+++ bfield_gpu.cuh
@@ -3,6 +3,6 @@
 
 __global__ void field_kernel(void);
 
-__global__ void magnetic_field(double* bx, double* by, double* bz, double* currents, const double* mfx, const double* mfy, const double* mfz, const int nseg);
+__global__ void magnetic_field(double* bx, double* by, double* bz, double* currents, const double* mfx, const double* mfy, const double* mfz, const int ncoil, const int nseg);
 
 #endif
