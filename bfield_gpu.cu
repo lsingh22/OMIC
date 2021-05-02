13041915290af0b3e0e1b9efe65002461040bc68
Luquant Singh <167-lsingh2@users.noreply.newton.msvc.wisc.edu>
2021-05-02 15:44:37 -0500

    Setup shared memory in field kernel, load start and end of filaments, and fix function argument error in host-side code

--- bfield_gpu.cu
+++ bfield_gpu.cu
@@ -3,23 +3,64 @@
 #include "bfield_gpu.cuh"
 
 __global__ void field_kernel(const double* mx, const double* my, const double* mz, 
-									  const double* curr, double* dbx, double* dby, double* dbz) {
+									  const double* curr, const double x, const double y, 
+                             const double z, double* dbx, double* dby, double* dbz) {
 
-	// Dynamically allocate shared memory
+	// Dynamically allocate shared memory for inputs, outputs
+   // Inputs are coil positions, currents, and rotation matrix
+   // Outputs are field contributions
+   extern volatile __shared__ double posx[];
+   extern volatile __shared__ double posy[];
+   extern volatile __shared__ double posz[];
+   extern volatile __shared__ double icoil[];
+   extern volatile __shared__ double rot_matrix[];
+   extern volatile __shared__ double shdbx[];
+   extern volatile __shared__ double shdby[];
+   extern volatile __shared__ double shdbz[];
 
-	// Loads from constant memory
+   // Define indexing variables 
+   int bid = blockIdx.x;
+   int seg = threadIdx.x; // for keeping track of segments
+   int cid = threadIdx.y; // for indexing current 
+   int idx = bid * seg * cid + seg * cid + seg; // position index
 
-	// Each thread loads in a filament position to shMem
+   // Global load of start of segment
+   double xi = mx[cid * seg + seg]; 
+   double yi = my[cid * seg + seg];
+   double zi = mz[cid * seg + seg];
+
+   // Write starting segments to shared memory 
+   posx[cid * seg + seg] = xi;
+   posy[cid * seg + seg] = yi;
+   posz[cid * seg + seg] = zi;
+
+   // Share segment starts to hide latency in loading neighbor segment
+	__syncthreads();
+
+   // Shared load of end of segment
+   double xf = posx[cid * seg + seg + 1]; 
+   double yf = posy[cid * seg + seg + 1];
+   double zf = posz[cid * seg + seg + 1]; 
+	
+   // Using start and end segments, and kernel arguments, compute cartesian dB
+   // Parameters are defined as in Hanson & Hirschman (TODO:ref)
+
+   // for loop over nfp
+   // for(int ip = 1; i < nfp; i++) {
+
+
+      // update local bx,by,bz
+   // }
+      
+   // Load segment dB to shared memory to perform block reduction
+   // Swapped double buffer method is performed for each cartesian component
+   //shdbx[cid * seg + seg] = bx;
+   //shdby[cid * seg + seg] = by;
+   //shdbz[cid * seg + seg] = bz;
 
 	// __syncthreads();
 
-	// Calculate start and end vectors and write to shMem
-
-	// __syncthreads();
-
-	// Calculate segment length, unit vector, and cross product prefactor
-
-	// Calculate magnetic field contribution and store in shMem
+   // Perform Hillis-Steele reduction and store in global db (x,y,z)   
 
 }
 
@@ -27,7 +68,7 @@ __global__ void hillis_steele(double* dBx, double* dBy, double* dBz,
 								  double* bx,  double* by,  double* bz);
 
 extern "C" void magnetic_field(const double* mfx, const double* mfy, const double* mfz, 
-                             const double* currents, const int ncoil, const int nseg, const int size_fp) {
+                               const double* currents, const int ncoil, const int nseg, const int size_fp) {
 
 	// TODO: for purposes of parallel implementation, ncoil should be Ncoil * Nfils
 
@@ -45,7 +86,7 @@ extern "C" void magnetic_field(const double* mfx, const double* mfy, const doubl
 	unsigned int DimGrid = (ncoil + coils_per_block - 1) / coils_per_block;
 	unsigned int field_shMem_size = (segs_per_block * 3 + 2 * (segs_per_block - 1) * 3 \
 										      + coils_per_block) * sizeof(double);
-	unsigned int scan_shMem_size = 2 * DimGrid * 3 * sizeof(double); 
+// unsigned int scan_shMem_size = 2 * DimGrid * 3 * sizeof(double); 
 
 	// Create helper currents and reduction buffer unified arrays
 	double* Icoil;
@@ -65,7 +106,9 @@ extern "C" void magnetic_field(const double* mfx, const double* mfy, const doubl
 	for(int i = 0; i < size_fp; i++) {
 		
 		// Call field kernel to get magnetic field
-		field_kernel<<<DimGrid, DimBlock, field_shMem_size>>>(mfx, mfy, mfz, Icoil, dBx, dBy, dBz);
+		field_kernel<<<DimGrid, DimBlock, field_shMem_size>>>(mfilx, mfily, mfilz, Icoil, 
+                                                            xsurf[i], ysurf[i], zsurf[i],
+                                                            dBx, dBy, dBz);
 		cudaDeviceSynchronize(); // synchronize to prepare for reduction
 		
 		//Call Hillis_Steele kernel to scan the partial sums and get bx, by, bz
